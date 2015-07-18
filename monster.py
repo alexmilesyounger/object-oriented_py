5a82b0efb1acf8d9596c98d2b19788707c418c58
Alex Miles Younger <alex@alexmilesyounger.com>
2015-07-18 18:17:29 -0400

    Create subclasses of monsters

--- monster.py
+++ monster.py
@@ -3,7 +3,7 @@ import random
 COLORS = ['yellow', 'red', 'blue', 'green']
 
 
-class Monster(object): # by explicitly stating the object in the Monster class, the class more easily works in Python 2 and Python 3.
+class Monster(object): # by explicitly stating the object in the Monster class, the class more easily works in Python 2 and Python 3 at the same time.
 	min_hit_points = 1
 	max_hit_points = 1
 	min_experience = 1
@@ -25,4 +25,23 @@ class Monster(object): # by explicitly stating the object in the Monster class, 
 
 # Usually you limit yourself to one class per file, but in the case of subclasses it's less of a problem. 
 
+
 class Goblin(Monster): # by putting Monster in as an argument, this tells python that Goblin is a subclass of Monster and that it automatically inherits all of the Monster attributes. 
+	max_hit_points = 3
+	max_experience = 2
+	sound = 'squeak'
+
+
+class Troll(Monster):
+	min_hit_points = 3
+	max_hit_points = 5
+	min_experience = 2
+	max_experience = 6
+	sound = 'growl'
+
+class Dragon(Monster):
+	min_hit_points = 5
+	max_hit_points = 10
+	min_experience = 6
+	max_experience = 10
+	sound = 'roaaaaaaaar'
